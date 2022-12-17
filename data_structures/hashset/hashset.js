09109b1836eba0e8e19940913d8c4e73e464a0f6
levi-blodgett <levi_blodgett@outlook.com>
2022-12-16 17:37:40 -0700

    Finished initial learning of DS

---
+++ data_structures/hashset/hashset.js
@@ -0,0 +1,27 @@
+// A data structure that stores a collection of values
+// A hashset does not maintain the order of its elements and does not allow duplicate values.
+// Faster than arrays at finding out if an element is contained.
+
+// EXAMPLE
+
+const hashSet = new Set();
+
+// Add elements to the hash set
+hashSet.add(1);
+hashSet.add(2);
+hashSet.add(3);
+
+// Check if an element is in the hash set
+console.log(hashSet.has(1)); // outputs true
+console.log(hashSet.has(4)); // outputs false
+
+// Remove an element from the hash set
+hashSet.delete(2);
+
+// Iterate over the elements in the hash set
+for (const value of hashSet) {
+    console.log(value);
+}
+// Output:
+// 1
+// 3
