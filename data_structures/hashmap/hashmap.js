1e7bbd8c485f0ad6ee6bd6d7197bd784ddfdd138
levi-blodgett <levi_blodgett@outlook.com>
2022-12-19 13:42:44 -0700

    Updated comments on file

--- data_structures/hashmap/hashmap.js
+++ data_structures/hashmap/hashmap.js
@@ -11,16 +11,16 @@ hashmap.set('key1', 'value1');
 hashmap.set('key2', 'value2');
 hashmap.set('key3', 'value3');
 
-console.log(hashmap); // The hashmap now contains: {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+console.log(hashmap); // {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
 // Retrieve a value from the hashmap using its key
 const value1 = hashmap.get('key1');
-console.log("Value1 is: " + value1); // value1 is: 'value1'
+console.log(value1); // 'value1'
 
 // Update a value in the hashmap
 hashmap.set('key1', 'newValue1');
-console.log("key1 is: " + hashmap.get('key1')); // key1 is: newValue1
+console.log(hashmap.get('key1')); // newValue1
 
 // Remove a key-value pair from the hashmap
 hashmap.delete('key1');
-console.log(hashmap); // The hashmap now contains: {'key2': 'value2', 'key3': 'value3'}
+console.log(hashmap); // {'key2': 'value2', 'key3': 'value3'}
