873860660d21085a350d8bf0af19e43712f40ef6
levi-blodgett <levi_blodgett@outlook.com>
2022-12-19 13:44:49 -0700

    Updated logic in file

--- data_structures/hashmap/1 - Two Sum.js
+++ data_structures/hashmap/1 - Two Sum.js
@@ -2,32 +2,22 @@
 const numbers = [2, 7, 11, 15];
 const target = 22;
 
-// Define the Solution class
-class Solution {
-  // Define the twoSum function
-  twoSum(numbers, target) {
-    // Initialize an empty hashmap
-    const hashmap = new Map();
-    // Iterate over the numbers
-    for (let i = 0; i < numbers.length; i++) {
-      // Compute the complement
-      const complement = target - numbers[i];
-      // Check if the complement exists in the hashmap
-      if (hashmap.get(complement)) {
-        // Return the indices of the two numbers
-        return [i, hashmap.get(complement)];
-      }
-      // Add the current number and its index to the hashmap
-      hashmap.set(numbers[i], i);
+function twoSum(numbers, target) {
+  // Initialize an empty hashmap
+  const hashmap = new Map();
+  // Iterate over the numbers
+  for (let i = 0; i < numbers.length; i++) {
+    // Compute the complement
+    const complement = target - numbers[i];
+    // Check if the complement exists in the hashmap
+    if (hashmap.get(complement)) {
+      // Return the indices of the two numbers
+      return [i, hashmap.get(complement)];
     }
+    // Add the current number and its index to the hashmap
+    hashmap.set(numbers[i], i);
   }
 }
 
-// Create a new Solution object
-const solution = new Solution();
-
-// Call the twoSum function
-const result = solution.twoSum(numbers, target);
-
 // Print the result to the console
-console.log(result);
+console.log(twoSum(numbers, target));
